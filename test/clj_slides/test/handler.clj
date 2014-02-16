4d65abdcc7202d3ef2627658c8c858ca87456c45
xorphitus <xorphitus@gmail.com>
2014-02-16 23:30:05 +0900

    #1 testがコケるので修正

--- test/clj_slides/test/handler.clj
+++ test/clj_slides/test/handler.clj
@@ -1,14 +1,17 @@
 (ns clj-slides.test.handler
   (:use clojure.test
-        ring.mock.request  
+        ring.mock.request
         clj-slides.handler))
 
 (deftest test-app
   (testing "main route"
     (let [response (app (request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
-  
+      (is (= (:status response) 200))))
+
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+      (is (= (:status response) 404))))
+
+  (testing "publish"
+    (let [html (publish "T_I_T_L_E" "D_E_S_C" "A_U_T_H_O_E_R" [:div "test"])]
+      (is (re-find #"<title>T_I_T_L_E</title>" html)))))
